ac718379b60614f01985b511ffa911a2f119887f
grp2 user <grp2@example.com>
2022-07-06 15:00:12 +0200

    First commit

---
+++ app/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$servername = "db";
+$username = "root";
+$pass = $_ENV["USER_PASS"];
+
+// Create connection
+$conn = new mysqli($servername, $username, $pass);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$db_list = mysqli_query($conn, "SHOW DATABASES");
+
+echo "<h1>Liste des bases de donnee:</h1>";
+
+echo "<ul>";
+while ($row = mysqli_fetch_array($db_list)) {
+    echo "<li>" . "$row[0]" . "</li>";
+}
+echo "</ul>";
+?>
+
